395f20bebe671023f228aef62467e4786a93b6e0
Marco Bruno <marco.bruno.br@gmail.com>
2021-04-26 14:06:15 +0200

    :package: Remove sucrase

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
-import dontenv from "dotenv";
-import Discord from "discord.js";
-
-dontenv.config();
+require("dotenv").config();
+const Discord = require("discord.js");
 
 const bot = new Discord.Client();
 const { TOKEN } = process.env;
